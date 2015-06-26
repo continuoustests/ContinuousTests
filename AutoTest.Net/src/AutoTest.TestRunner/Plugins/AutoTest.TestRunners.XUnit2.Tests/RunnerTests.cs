6b5ab98fd83a9984767d14b821566f85fd6b8c74
thefringeninja <joao.paul@braganca.name>
2015-06-26 13:46:09 +0200

    uncommented out tests, all are failing

--- AutoTest.Net/src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.XUnit2.Tests/RunnerTests.cs
+++ AutoTest.Net/src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.XUnit2.Tests/RunnerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using AutoTest.TestRunners.Shared.Options;
 using AutoTest.TestRunners.Shared.Plugins;
@@ -25,31 +26,31 @@ namespace AutoTest.TestRunners.XUnit2.Tests
     [TestFixture]
     public class RunnerTests
     {
-        //private AppDomain _childDomain = null;
-        //private Plugin _plugin = null;
-        //private ITestRunner _runner;
+        private AppDomain _childDomain = null;
+        private Plugin _plugin = null;
+        private ITestRunner _runner;
 
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    AppDomainSetup domainSetup = new AppDomainSetup()
-        //    {
-        //        ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-        //        ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile,
-        //        ApplicationName = AppDomain.CurrentDomain.SetupInformation.ApplicationName,
-        //        LoaderOptimization = LoaderOptimization.MultiDomainHost
-        //    };
-        //    _childDomain = AppDomain.CreateDomain("XUnit app domain", null, domainSetup);
-        //    _plugin = new Plugin(typeof(Runner).Assembly.Location, typeof(Runner).FullName);
-        //    _runner = (ITestRunner)_childDomain.CreateInstanceAndUnwrap(typeof(TestRunner).Assembly.FullName, typeof(TestRunner).FullName);
-        //}
+        [SetUp]
+        public void SetUp()
+        {
+            AppDomainSetup domainSetup = new AppDomainSetup()
+            {
+                ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile,
+                ApplicationName = AppDomain.CurrentDomain.SetupInformation.ApplicationName,
+                LoaderOptimization = LoaderOptimization.MultiDomainHost
+            };
+            _childDomain = AppDomain.CreateDomain("XUnit2 app domain", null, domainSetup);
+            _plugin = new Plugin(typeof(Runner).Assembly.Location, typeof(Runner).FullName);
+            _runner = (ITestRunner)_childDomain.CreateInstanceAndUnwrap(typeof(TestRunner).Assembly.FullName, typeof(TestRunner).FullName);
+        }
 
-        //[TearDown]
-        //public void Teardown()
-        //{
-        //    if (_childDomain != null)
-        //        AppDomain.Unload(_childDomain);
-        //}
+        [TearDown]
+        public void Teardown()
+        {
+            if (_childDomain != null)
+                AppDomain.Unload(_childDomain);
+        }
 
         [Test]
         public void Should_identify_test()
@@ -83,77 +84,75 @@ namespace AutoTest.TestRunners.XUnit2.Tests
             return Path.Combine(path, relativePath);
         }
 
-        //[Test]
-        //public void Should_run_tests()
-        //{
-        //    var options = new RunnerOptions("XUnit");
-        //    options.AddAssembly(new AssemblyOptions(
-        //        Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll")));
+        [Test]
+        public void Should_run_tests()
+        {
+            var options = new RunSettings(new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll")), new string[0], null);
 
-        //    var result = _runner.Run(_plugin, options);
+            var result = _runner.Run(_plugin, options);
 
-        //    Assert.That(result.Count(), Is.EqualTo(7));
-        //    var test1 = result.Where(x => x.TestName.Equals("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass")).First();
-        //    Assert.That(test1.Runner, Is.EqualTo("XUnit"));
-        //    Assert.That(test1.Assembly, Is.EqualTo(options.Assemblies.ElementAt(0).Assembly));
-        //    Assert.That(test1.TestFixture, Is.EqualTo(""));
-        //    Assert.That(test1.DurationInMilliseconds, Is.GreaterThan(0));
-        //    Assert.That(test1.TestName, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass"));
-        //    Assert.That(test1.State, Is.EqualTo(Shared.Results.TestState.Passed));
+            Assert.That(result.Count(), Is.EqualTo(7));
+            var test1 = result.Where(x => x.TestName.Equals("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass")).First();
+            Assert.That(test1.Runner, Is.EqualTo("XUnit"));
+            Assert.That(test1.Assembly, Is.EqualTo(options.Assembly));
+            Assert.That(test1.TestFixture, Is.EqualTo(""));
+            Assert.That(test1.DurationInMilliseconds, Is.GreaterThan(0));
+            Assert.That(test1.TestName, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass"));
+            Assert.That(test1.State, Is.EqualTo(Shared.Results.TestState.Passed));
 
-        //    var test2 = result.Where(x => x.TestName.Equals("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail")).First();
-        //    Assert.That(test2.Runner, Is.EqualTo("XUnit"));
-        //    Assert.That(test2.Assembly, Is.EqualTo(options.Assemblies.ElementAt(0).Assembly));
-        //    Assert.That(test2.TestFixture, Is.EqualTo(""));
-        //    Assert.That(test2.TestName, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail"));
-        //    Assert.That(test2.State, Is.EqualTo(Shared.Results.TestState.Failed));
-        //    Assert.That(test2.StackLines.Count(), Is.EqualTo(1));
-        //    Assert.That(test2.StackLines.ElementAt(0).Method, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail()"));
-        //    // Only verify if build with debug
-        //    if (test2.StackLines.ElementAt(0).File.Length > 0)
-        //    {
-        //        Assert.IsTrue(File.Exists(test2.StackLines.ElementAt(0).File));
-        //        Assert.That(test2.StackLines.ElementAt(0).Line, Is.EqualTo(23));
-        //    }
-        //}
+            var test2 = result.Where(x => x.TestName.Equals("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail")).First();
+            Assert.That(test2.Runner, Is.EqualTo("XUnit2"));
+            Assert.That(test2.Assembly, Is.EqualTo(options.Assembly));
+            Assert.That(test2.TestFixture, Is.EqualTo(""));
+            Assert.That(test2.TestName, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail"));
+            Assert.That(test2.State, Is.EqualTo(Shared.Results.TestState.Failed));
+            Assert.That(test2.StackLines.Count(), Is.EqualTo(1));
+            Assert.That(test2.StackLines.ElementAt(0).Method, Is.EqualTo("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_fail()"));
+            // Only verify if build with debug
+            if (test2.StackLines.ElementAt(0).File.Length > 0)
+            {
+                Assert.IsTrue(File.Exists(test2.StackLines.ElementAt(0).File));
+                Assert.That(test2.StackLines.ElementAt(0).Line, Is.EqualTo(23));
+            }
+        }
 
-        //[Test]
-        //public void Should_run_single_test()
-        //{
-        //    var options = new RunnerOptions("XUnit");
-        //    var assembly = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
-        //    assembly.AddTest("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass");
-        //    options.AddAssembly(assembly);
+        [Test]
+        public void Should_run_single_test()
+        {
+            var assemblyOptions = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
+            assemblyOptions.AddTest("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1.Should_pass");
 
-        //    var result = _runner.Run(_plugin, options);
+            var options = new RunSettings(assemblyOptions, new string[0], null);
 
-        //    Assert.That(result.Count(), Is.EqualTo(1));
-        //}
+            var result = _runner.Run(_plugin, options);
 
-        //[Test]
-        //public void Should_run_members()
-        //{
-        //    var options = new RunnerOptions("XUnit");
-        //    var assembly = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
-        //    assembly.AddMember("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1");
-        //    options.AddAssembly(assembly);
+            Assert.That(result.Count(), Is.EqualTo(1));
+        }
 
-        //    var result = _runner.Run(_plugin, options);
+        [Test]
+        public void Should_run_members()
+        {
+            var assemblyOptions = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
+            assemblyOptions.AddMember("AutoTest.TestRunners.XUnit.Tests.TestResource.Class1");
 
-        //    Assert.That(result.Count(), Is.EqualTo(2));
-        //}
+            var options = new RunSettings(assemblyOptions, new string[0], null);
 
-        //[Test]
-        //public void Should_run_namespace()
-        //{
-        //    var options = new RunnerOptions("XUnit");
-        //    var assembly = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
-        //    assembly.AddNamespace("AutoTest.TestRunners.XUnit.Tests.TestResource.Anothernamespace");
-        //    options.AddAssembly(assembly);
+            var result = _runner.Run(_plugin, options);
 
-        //    var result = _runner.Run(_plugin, options);
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
 
-        //    Assert.That(result.Count(), Is.EqualTo(4));
-        //}
+        [Test]
+        public void Should_run_namespace()
+        {
+            var assemblyOptions = new AssemblyOptions(Path.GetFullPath(@"AutoTest.TestRunners.XUnit.Tests.TestResource.dll"));
+            assemblyOptions.AddNamespace("AutoTest.TestRunners.XUnit.Tests.TestResource.Anothernamespace");
+
+            var options = new RunSettings(assemblyOptions, new string[0], null);
+
+            var result = _runner.Run(_plugin, options);
+
+            Assert.That(result.Count(), Is.EqualTo(4));
+        }
     }
 }
