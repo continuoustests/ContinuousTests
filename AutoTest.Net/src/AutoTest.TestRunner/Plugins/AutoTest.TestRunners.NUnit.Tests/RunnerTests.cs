5dff35a719395586251de55dfd9c4fd4e9142b45
acken <acken@acken.no>
2015-06-26 15:00:25 +0200

    Uncommented nunit tests

--- AutoTest.Net/src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.NUnit.Tests/RunnerTests.cs
+++ AutoTest.Net/src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.NUnit.Tests/RunnerTests.cs
@@ -13,47 +13,19 @@ using System.Reflection;
 
 namespace AutoTest.TestRunners.NUnit.Tests
 {
-    interface ITestRunner
-    {
-        IEnumerable<TestResult> Run(Plugin plugin, RunSettings options);
-    }
-
-    class TestRunner : MarshalByRefObject, ITestRunner
-    {
-        public IEnumerable<TestResult> Run(Plugin plugin, RunSettings options)
-        {
-            return plugin.New().Run(options);
-        }
-    }
-
     [TestFixture]
     public class RunnerTests
     {
-        //private AppDomain _childDomain = null;
-        //private Plugin _plugin = null;
-        //private ITestRunner _runner;
+        private Plugin _plugin = null;
+        private IAutoTestNetTestRunner _runner;
 
-        //[TestFixtureSetUp]
-        //public void SetUp()
-        //{
-        //    AppDomainSetup domainSetup = new AppDomainSetup()
-        //    {
-        //        ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-        //        ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile,
-        //        ApplicationName = AppDomain.CurrentDomain.SetupInformation.ApplicationName,
-        //        LoaderOptimization = LoaderOptimization.MultiDomainHost
-        //    };
-        //    _childDomain = AppDomain.CreateDomain("NUnit app domain", null, domainSetup);
-        //    _plugin = new Plugin(typeof(Runner).Assembly.Location, typeof(Runner).FullName);
-        //    _runner = (ITestRunner)_childDomain.CreateInstanceAndUnwrap(typeof(TestRunner).Assembly.FullName, typeof(TestRunner).FullName);
-        //}
-
-        //[TestFixtureTearDown]
-        //public void Teardown()
-        //{
-        //    if (_childDomain != null)
-        //        AppDomain.Unload(_childDomain);
-        //}
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            int a = 3;
+            _plugin = new Plugin(typeof(Runner).Assembly.Location, typeof(Runner).FullName);
+            _runner = _plugin.New();
+        }
 
         [Test]
         public void Should_recognize_test()
@@ -114,78 +86,82 @@ namespace AutoTest.TestRunners.NUnit.Tests
             Assert.That(runner.IsTest(assembly, method), Is.True);
         }
 
-        //[Test]
-        //public void Should_run_test()
-        //{
-        //    var options = new RunnerOptions("nunit");
-        //    options.AddAssembly(new AssemblyOptions(
-        //        Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    var results = _runner.Run(_plugin, options);i
-        //    Assert.That(results.Count(), Is.EqualTo(8));
+        [Test]
+        public void Should_run_test()
+        {
+            var assembly = new AssemblyOptions(
+                Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")
+            );
+            var options = new RunSettings(assembly, new string[] {}, "feedback_pipe");
+            var results = _runner.Run(options);
+            Assert.That(results.Count(), Is.EqualTo(9));
 
-        //    Assert.That(results.ElementAt(0).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    Assert.That(results.ElementAt(0).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
-        //    Assert.That(results.ElementAt(0).State, Is.EqualTo(Shared.Results.TestState.Failed));
-        //    Assert.That(results.ElementAt(0).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail()"));
-        //    Assert.That(results.ElementAt(0).StackLines.Count(), Is.EqualTo(1));
-        //    Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail()"));
+            Assert.That(results.ElementAt(0).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
+            Assert.That(results.ElementAt(0).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
+            Assert.That(results.ElementAt(0).State, Is.EqualTo(Shared.Results.TestState.Failed));
+            Assert.That(results.ElementAt(0).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail()"));
+            Assert.That(results.ElementAt(0).StackLines.Count(), Is.EqualTo(1));
+            Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail()"));
 
-        //    Assert.That(results.ElementAt(1).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    Assert.That(results.ElementAt(1).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
-        //    Assert.That(results.ElementAt(1).State, Is.EqualTo(Shared.Results.TestState.Ignored));
-        //    Assert.That(results.ElementAt(1).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore"));
-        //    Assert.That(results.ElementAt(1).StackLines.Count(), Is.EqualTo(1));
-        //    Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore()"));
+            Assert.That(results.ElementAt(1).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
+            Assert.That(results.ElementAt(1).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
+            Assert.That(results.ElementAt(1).State, Is.EqualTo(Shared.Results.TestState.Ignored));
+            Assert.That(results.ElementAt(1).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore"));
+            Assert.That(results.ElementAt(1).StackLines.Count(), Is.EqualTo(1));
+            Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore()"));
 
-        //    Assert.That(results.ElementAt(2).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    Assert.That(results.ElementAt(2).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
-        //    Assert.That(results.ElementAt(2).State, Is.EqualTo(Shared.Results.TestState.Passed));
-        //    Assert.That(results.ElementAt(2).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_pass"));
-        //    Assert.That(results.ElementAt(2).StackLines.Count(), Is.EqualTo(0));
+            Assert.That(results.ElementAt(2).Assembly, Is.EqualTo(Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
+            Assert.That(results.ElementAt(2).TestFixture, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1"));
+            Assert.That(results.ElementAt(2).State, Is.EqualTo(Shared.Results.TestState.Passed));
+            Assert.That(results.ElementAt(2).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_pass"));
+            Assert.That(results.ElementAt(2).StackLines.Count(), Is.EqualTo(0));
 
-        //    // For some reason NUnit can't seems to locate filename and line number on windows when stacktrace only shows method name
-        //    if (OS.IsPosix)
-        //    {
-        //        Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
-        //        Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Line, Is.EqualTo(23));
+            // For some reason NUnit can't seems to locate filename and line number on windows when stacktrace only shows method name
+            if (OS.IsPosix)
+            {
+                Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
+                Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Line, Is.EqualTo(22));
 
-        //        Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
-        //        Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Line, Is.EqualTo(30));
-        //    }
-        //}
+                Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
+                Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Line, Is.EqualTo(29));
+            }
+        }
 
-        //[Test]
-        //public void Should_run_tests_from_type()
-        //{
-        //    var options = new RunnerOptions("nunit");
-        //    options.AddAssembly(new AssemblyOptions(
-        //        Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    options.Assemblies.ElementAt(0).AddMember("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture2");
-        //    var results = _runner.Run(_plugin, options);
-        //    Assert.That(results.Count(), Is.EqualTo(2));
-        //}
+        [Test]
+        public void Should_run_tests_from_type()
+        {
+            var assembly = new AssemblyOptions(
+                Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")
+            );
+            assembly.AddMember("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture2");
+            var options = new RunSettings(assembly, new string[] {}, "feedback_pipe");
+            var results = _runner.Run(options);
+            Assert.That(results.Count(), Is.EqualTo(2));
+        }
 
-        //[Test]
-        //public void Should_run_tests_from_namespace()
-        //{
-        //    var options = new RunnerOptions("nunit");
-        //    options.AddAssembly(new AssemblyOptions(
-        //        Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    options.Assemblies.ElementAt(0).AddNamespace("AutoTest.TestRunners.NUnit.Tests.TestResource");
-        //    var results = _runner.Run(_plugin, options);
-        //    Assert.That(results.Count(), Is.EqualTo(5));
-        //}
+        [Test]
+        public void Should_run_tests_from_namespace()
+        {
+            var assembly = new AssemblyOptions(
+                Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")
+            );
+            assembly.AddNamespace("AutoTest.TestRunners.NUnit.Tests.TestResource");
+            var options = new RunSettings(assembly, new string[] {}, "feedback_pipe");
+            var results = _runner.Run(options);
+            Assert.That(results.Count(), Is.EqualTo(9));
+        }
 
-        //[Test]
-        //public void Should_run_single_test_and_type()
-        //{
-        //    var options = new RunnerOptions("nunit");
-        //    options.AddAssembly(new AssemblyOptions(
-        //        Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")));
-        //    options.Assemblies.ElementAt(0).AddTest("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore");
-        //    options.Assemblies.ElementAt(0).AddMember("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture2");
-        //    var results = _runner.Run(_plugin, options);
-        //    Assert.That(results.Count(), Is.EqualTo(3));
-        //}
+        [Test]
+        public void Should_run_single_test_and_type()
+        {
+            var assembly = new AssemblyOptions(
+                Path.GetFullPath(@"AutoTest.TestRunners.NUnit.Tests.TestResource.dll")
+            );
+            assembly.AddTest("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore");
+            assembly.AddMember("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture2");
+            var options = new RunSettings(assembly, new string[] {}, "feedback_pipe");
+            var results = _runner.Run(options);
+            Assert.That(results.Count(), Is.EqualTo(3));
+        }
     }
 }
