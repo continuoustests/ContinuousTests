0866abc2642b2c3e78e5aff7cfaa73e032d0aa2f
thefringeninja <joao.paul@braganca.name>
2015-06-29 11:03:41 +0200

    much simpler build system for windows

--- AutoTest.Net/build.bat
+++ AutoTest.Net/build.bat
@@ -11,12 +11,14 @@ if '%1' == '?' goto usage
 if '%1' == '/help' goto usage
 
 SET DIR=%~d0%~p0%
-SET NANT="%DIR%lib\Nant\nant.exe"
-SET build.config.settings="%DIR%settings\UppercuT.config"
-SET buildx86.config.settings="%DIR%settings\UppercuTx86.config"
+SET SOURCEDIR=%DIR%
+SET BINARYDIR="%DIR%build_outputAnyCPU"
+SET BINARYDIRx86="%DIR%build_outputx86"
+SET DEPLOYDIR="%DIR%ReleaseBinaries"
 
-%NANT% %1 /f:.\build\default.build -D:build.config.settings=%buildx86.config.settings%
-%NANT% %1 /f:.\build\default.build -D:build.config.settings=%build.config.settings%
+
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %SOURCEDIR%\AutoTest.NET.sln /property:OutDir=%BINARYDIR%\AutoTest.NET;Configuration=Release /target:rebuild
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %SOURCEDIR%\AutoTest.TestRunner.sln /property:OutDir=%BINARYDIRx86%\AutoTest.TestRunner;Configuration=Release /target:rebuild /p:Platform=x86
 
 if %ERRORLEVEL% NEQ 0 goto errors
 
